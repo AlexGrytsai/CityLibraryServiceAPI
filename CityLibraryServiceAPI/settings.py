b4704cb6cc34f34112a798e42658263a9cf4e4d1
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-13 17:39:25 +0300

    Added the ability to switch between PostgreSQL & SQLite in the settings

--- CityLibraryServiceAPI/settings.py
+++ CityLibraryServiceAPI/settings.py
@@ -77,12 +77,26 @@ WSGI_APPLICATION = "CityLibraryServiceAPI.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+POSTGRESQL_DB = False
+
+if POSTGRESQL_DB:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
