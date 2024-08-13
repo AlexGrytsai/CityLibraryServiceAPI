bc5d33af6abb585bc582d218a49dfcf08a4ad83d
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-13 18:07:54 +0300

    fixed all PEPs problems with black

--- CityLibraryServiceAPI/wsgi.py
+++ CityLibraryServiceAPI/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CityLibraryServiceAPI.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "CityLibraryServiceAPI.settings"
+)
 
 application = get_wsgi_application()
