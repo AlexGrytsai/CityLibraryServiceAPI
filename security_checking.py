df0e799ca9ae9bbf35d306739164c2fd2239833e
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-14 13:34:58 +0300

    changed print() -> logger in security_checking.py

--- security_checking.py
+++ security_checking.py
@@ -1,4 +1,19 @@
+import logging
+import os
 import subprocess
+
+from django.core.management import execute_from_command_line
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logger = logging.getLogger("django")
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "CityLibraryServiceAPI.settings"
+)
+execute_from_command_line(["manage.py", "check"])
 
 
 def run_bandit() -> None:
@@ -18,6 +33,7 @@ def run_bandit() -> None:
     ]
 
     try:
+        logger.info("Running bandit scan...")
         result = subprocess.run(
             command,
             check=True,
@@ -25,33 +41,38 @@ def run_bandit() -> None:
             stderr=subprocess.PIPE,
             text=True,
         )
-        print(
-            f"Bandit scan completed successfully. "
-            f"Report saved to 'report_bandit.{format_output}'."
+        logger.info(
+            "Bandit scan completed successfully. "
+            "Report saved to 'report_bandit.{format_output}'."
         )
     except subprocess.CalledProcessError as e:
-        print(f"Error during Bandit scan:\n{e.stderr}")
+        logger.error(f"Error during Bandit scan: {e}")
 
 
 def run_safety_check() -> None:
-    # safety scan --output html --save-html output.html
+    # safety --stage production scan --key=<API_KEY> --output html --save-html output.html
+    api_key = os.environ.get("SAFETY_API_KEY")
     command = [
         "safety",
+        "--stage", "production",
         "scan",
+        f"--key={api_key}",
         "--output",
         "html",
         "--save-html",
         "output.html",
     ]
 
-    print("Running safety scan...")
-    with open("report_safety.html", "w") as f:
-        subprocess.run(command, stdout=f, stderr=subprocess.STDOUT)
-
-    print(
-        "Safety scan completed successfully. "
-        "Report saved to 'report_safety.html'."
-    )
+    logger.info("Running safety scan...")
+    try:
+        with open("report_safety.html", "w") as f:
+            subprocess.run(command, stdout=f, stderr=subprocess.STDOUT)
+        logger.info(
+            "Safety scan completed successfully. "
+            "Report saved to 'report_safety.html'."
+        )
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error during safety scan: {e}")
 
 
 if __name__ == "__main__":
