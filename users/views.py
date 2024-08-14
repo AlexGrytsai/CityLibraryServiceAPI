a32b60e677cd57ee6cd6388cf8163ee6053703e3
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-14 09:42:29 +0300

    created UserCreateView for register a new users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from users.serializers import UserCreateSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    """
+    UserCreateView is a class-based view that inherits from
+    generics.CreateAPIView.
+    It is used to handle the creation of new User instances.
+
+    This view uses the UserCreateSerializer to validate and serialize
+    the data from the request.
+    It allows any user to create a new User instance, as specified
+    by the AllowAny permission class.
+    """
+
+    serializer_class = UserCreateSerializer
+    permission_classes = (AllowAny,)
