082a159377b00c6f8fc2d4f1792a1c622fade8a6
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-15 10:47:08 +0300

    created a BorrowingView

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from borrowing.serializers import BorrowingSerializer
+
+
+class BorrowingView(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = (IsAdminUser,)
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return BorrowingSerializer
