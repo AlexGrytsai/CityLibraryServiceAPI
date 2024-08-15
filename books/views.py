86ec558d43dbf5725a5cefc2191bacfa3e94a5e5
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-15 07:09:11 +0300

    created a BookViewSet

--- books/views.py
+++ books/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
 
-# Create your views here.
+from books.models import Book
+from books.serializers import BookSerializer, BookListSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard actions to manage books in the system.
+    """
+
+    queryset = Book.objects.all()
+
+    def get_permissions(self):
+        if self.request.method in ["POST", "PUT", "PATCH", "DELETE"]:
+            return (IsAdminUser(),)
+        return (AllowAny(),)
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BookListSerializer
+        return BookSerializer
+
+    def get_queryset(self):
+        if self.action == "retrieve":
+            return Book.objects.filter(id=self.kwargs["pk"])
+        return super(BookViewSet, self).get_queryset()
