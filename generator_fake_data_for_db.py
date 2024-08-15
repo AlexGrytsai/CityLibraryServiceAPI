3a8c1979ea23818d5c63734a7fedda3c8983f7b5
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-15 08:48:41 +0300

    created two functions for open and save json files

--- generator_fake_data_for_db.py
+++ generator_fake_data_for_db.py
@@ -1,3 +1,5 @@
+import json
+
 from faker import Faker
 
 fake = Faker()
@@ -22,6 +24,23 @@ def generate_fake_user_data() -> list:
 
         users_data.append(user)
     return users_data
+
+
+def save_users_data_to_json(data: list, file_name: str) -> None:
+    """
+    Save a list of user data to a JSON file.
+    """
+    with open(f"{file_name}.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def open_json_file(file_name: str) -> list:
+    """
+    Open a JSON file and return its contents as a list of dictionaries.
+    """
+    with open(file_name, "r") as file:
+        data = json.load(file)
+    return data
 
 
 if __name__ == "__main__":
