b66c387d1de7844ef39ee4e359a754108353474d
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-15 10:14:31 +0300

    crated a Borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(
+        auto_now_add=True,
+        db_comment="Borrowing date of the book",
+        help_text="Borrowing date of the book",
+    )
+    expected_return_date = models.DateField(
+        db_comment="Expected return date of the book",
+        help_text="Expected return date of the book",
+    )
+    actual_return_date = models.DateField(
+        null=True,
+        blank=True,
+        db_comment="Actual return date of the book",
+        help_text="Actual return date of the book",
+    )
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.PROTECT,
+        db_comment="Book which the user is borrowed",
+        help_text="Book",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        db_comment="User who borrowed the book",
+        help_text="User",
+    )
+
+    class Meta:
+        ordering = ["-expected_return_date"]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["book", "user"], name="unique_borrowing"
+            ),
+            models.CheckConstraint(
+                name="expected_return_date_check",
+                check=models.Q(
+                    expected_return_date__gte=models.F("borrow_date")
+                ),
+            ),
+        ]
+
+    def __str__(self):
+        return f"{self.book.title} borrowed by {self.user}"
