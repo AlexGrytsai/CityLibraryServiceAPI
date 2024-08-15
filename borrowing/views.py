5ae87fc3e7a36e787854557c9b5fd5d7ed5881a7
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-15 10:55:20 +0300

    added perform_create with checking a book inventory and minus 1

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,5 +1,8 @@
-from rest_framework import viewsets, mixins
+from typing import Type
+
+from rest_framework import viewsets, mixins, serializers
 from rest_framework.permissions import IsAdminUser
+from rest_framework.serializers import Serializer
 
 from borrowing.serializers import BorrowingSerializer
 
@@ -7,6 +10,17 @@ from borrowing.serializers import BorrowingSerializer
 class BorrowingView(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = (IsAdminUser,)
 
-    def get_serializer_class(self):
-        if self.action == 'create':
+    def get_serializer_class(self) -> Type[Serializer]:
+        if self.action == "create":
             return BorrowingSerializer
+
+    def perform_create(self, serializer) -> None:
+        book = serializer.validated_data["book"]
+
+        if book.inventory == 0:
+            raise serializers.ValidationError(
+                {f"{book.title}": "The book is not available"}
+            )
+
+        book.inventory -= 1
+        book.save()
