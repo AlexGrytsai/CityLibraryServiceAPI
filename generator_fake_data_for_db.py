9303434bb6b3f83f8a2154e2ed0cf184212be5ae
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-15 09:19:02 +0300

    created a generate_fake_books_data_for_db and data_fusion functions

--- generator_fake_data_for_db.py
+++ generator_fake_data_for_db.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from faker import Faker
 
@@ -26,6 +27,39 @@ def generate_fake_user_data() -> list:
     return users_data
 
 
+def generate_fake_books_data_for_db() -> list:
+    books_data = []
+    for i in range(10):
+        pk = i + 30
+        title = fake.unique.sentence(nb_words=4)
+        author = fake.name()
+        daily_fee = str(
+            fake.pydecimal(min_value=0.00, max_value=9.00, right_digits=2)
+        )
+        book = {
+            "model": "books.book",
+            "pk": pk,
+            "fields": {
+                "title": title,
+                "author": author,
+                "cover": random.choice(["HARD", "SOFT"]),
+                "inventory": random.randint(0, 10),
+                "daily_fee": daily_fee
+            },
+        }
+
+        books_data.append(book)
+    return books_data
+
+
+def data_fusion(*args, **kwargs) -> list:
+    new_data = []
+    for arg in args:
+        new_data.extend(arg)
+
+    return new_data
+
+
 def save_users_data_to_json(data: list, file_name: str) -> None:
     """
     Save a list of user data to a JSON file.
@@ -45,3 +79,8 @@ def open_json_file(file_name: str) -> list:
 
 if __name__ == "__main__":
     users = generate_fake_user_data()
+    books = generate_fake_books_data_for_db()
+
+    data_for_json = data_fusion(users, books)
+
+    save_users_data_to_json(data_for_json, "example_data_for_db")
