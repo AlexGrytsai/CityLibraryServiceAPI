4b8fe69e11b11a42a7346435e432447a488150eb
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-15 10:26:38 +0300

    created a BorrowingSerializer

---
+++ borrowing/serializers.py
@@ -0,0 +1,26 @@
+from django.db.models import Q
+from rest_framework import serializers
+
+from books.models import Book
+from borrowing.models import Borrowing
+from users.models import User
+
+
+class BorrowingSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+
+    book = serializers.PrimaryKeyRelatedField(
+        queryset=Book.objects.all().filter(Q(inventory__gt=0))
+    )
+
+    class Meta:
+        model = Borrowing
+        fields = [
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        ]
