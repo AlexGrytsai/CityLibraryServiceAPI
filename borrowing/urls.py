5d01a1085b23f802bc1d7dd8f107a7b5ef763993
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-16 07:06:23 +0300

    used a roter for BorrowingView in urls

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
 from borrowing.views import BorrowingView
 
+router = routers.DefaultRouter()
+
+router.register("borrowings", BorrowingView, basename="borrowing")
+
 urlpatterns = [
-    path("", BorrowingView.as_view({"post": "create"}), name="borrowing"),
+    path("", include(router.urls)),
 ]
 
 app_name = "borrowing"
