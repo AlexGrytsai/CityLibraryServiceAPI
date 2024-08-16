1e49669020b54b4865f2d3b7b34946309874206e
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-16 10:31:31 +0300

    Added action 'return' for BorrowingView. This action returned book

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,8 +1,12 @@
 import logging
+from datetime import datetime
 from typing import Type
 
-from rest_framework import viewsets, mixins, serializers
-from rest_framework.permissions import IsAuthenticated
+from django.http import HttpRequest
+from rest_framework.response import Response
+from rest_framework import viewsets, mixins, serializers, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.serializers import Serializer
 
 from borrowing.models import Borrowing
@@ -70,3 +74,43 @@ class BorrowingView(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
         book.save()
 
         serializer.save(user=self.request.user)
+
+    @action(
+        detail=True,
+        methods=["GET"],
+        url_path="return",
+        url_name="return",
+        permission_classes=(IsAdminUser,)
+    )
+    def return_book(self, request: HttpRequest, pk: int) -> Response:
+        borrowing = self.get_object()
+        if borrowing.actual_return_date is not None:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"message": "Book already returned"}
+            )
+
+        borrowing.actual_return_date = datetime.now()
+
+        logger.info(
+            f"Inventory until returning: {borrowing.book.inventory}"
+        )
+
+        borrowing.book.inventory += 1
+        borrowing.book.save()
+
+        logger.info(
+            f"Book (id={borrowing.book.id}) returned "
+            f"by user id={borrowing.user.id}. "
+            f"Inventory={borrowing.book.inventory})"
+        )
+
+        borrowing.save()
+        return Response(
+            status=status.HTTP_200_OK,
+            data={
+                "message":
+                    f"Book {borrowing.book.title} "
+                    f"(id={borrowing.book.id}) returned"
+            }
+        )
