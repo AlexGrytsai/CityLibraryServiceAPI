e77729813333879f067e588cccd9cb928f595252
Alex Grytsai <a.grytsai1987@gmail.com>
2024-08-16 10:04:13 +0300

    Added fiter parameters for BorrowingView. Added logger

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,11 +1,18 @@
+import logging
 from typing import Type
 
 from rest_framework import viewsets, mixins, serializers
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.serializers import Serializer
 
 from borrowing.models import Borrowing
-from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingDetailSerializer
+from borrowing.serializers import (
+    BorrowingSerializer,
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+logger = logging.getLogger("my_debug")
 
 
 class BorrowingView(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
@@ -14,6 +21,30 @@ class BorrowingView(mixins.CreateModelMixin, viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         current_user = self.request.user
+
+        user_id = self.request.query_params.get("user_id")
+        is_active = self.request.query_params.get("is_active")
+
+        if user_id and current_user.is_staff:
+            logger.info(
+                f"{current_user} used 'user_id'={user_id} "
+                f"for filter borrowed books"
+            )
+            return super().get_queryset().filter(user__id=user_id)
+        if is_active:
+            logger.info(
+                f"{current_user} used 'is_active'={is_active} "
+                f"for filter borrowed books"
+            )
+            if is_active.lower() == "true":
+                return (
+                    super()
+                    .get_queryset()
+                    .filter(actual_return_date__isnull=True)
+                )
+            return (
+                super().get_queryset().filter(actual_return_date__isnull=False)
+            )
         if current_user.is_staff:
             return super(BorrowingView, self).get_queryset()
         return super().get_queryset().filter(user=current_user)
