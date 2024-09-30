ebcac77b06a8f42df90464d394139ff6ca76a6a0
Alex Grytsai <a.grytsai1987@gmail.com>
2024-09-30 11:54:26 +0300

    Moved all telegram functions to telegram.py

--- notification/tasks.py
+++ notification/tasks.py
@@ -1,44 +1,17 @@
 import asyncio
 import logging
-import os
+from datetime import timedelta
 
 from celery import shared_task
+from dateutil.utils import today
 from dotenv import load_dotenv
-from telegram import Bot
+
+from borrowing.models import Borrowing
+from notification.telegram import TelegramNotification
 
 load_dotenv()
 
 logger = logging.getLogger("my_debug")
-
-telegram_token = os.getenv("TELEGRAM_TOKEN")
-if not telegram_token:
-    logger.error("TELEGRAM_TOKEN is not set")
-    raise ValueError("TELEGRAM_TOKEN is not set")
-
-try:
-    bot = Bot(token=telegram_token)
-except Exception as e:
-    logger.error(f"Telegram bot error: {e}")
-    raise
-
-
-def get_staff_users_chat_ids_list() -> list[int]:
-    staff_users_chat_ids = os.getenv("STAFFUSERS_CHAT_IDS").split(",")
-    if staff_users_chat_ids:
-        list_chat_ids = [
-            int(chat_id) for chat_id in staff_users_chat_ids if chat_id.strip()
-        ]
-        logger.info(f"Staff users chat ids: {list_chat_ids}")
-        return list_chat_ids
-    logger.info("Staff users chat ids: []")
-    return []
-
-
-async def send_telegram_message_to_staff_users(text: str) -> None:
-    staff_users = get_staff_users_chat_ids_list()
-    for chat_id in staff_users:
-        logger.info(f"Send message to chat id: {chat_id}")
-        await bot.send_message(chat_id=chat_id, text=text)
 
 
 @shared_task
@@ -46,4 +19,31 @@ def notify_new_borrowing(borrowing_details: str) -> None:
     message = f"New Borrowing: {borrowing_details}"
 
     logger.info("Start sending message to staff users")
-    asyncio.run(send_telegram_message_to_staff_users(message))
+    asyncio.run(
+        TelegramNotification().send_telegram_message_to_staff_users(message)
+    )
+
+@shared_task
+def notify_overdue_borrowings() -> None:
+    tomorrow_date = today() + timedelta(days=1)
+    overdue_borrowings = Borrowing.objects.filter(
+        actual_return_date=None, expected_return_date__lt=tomorrow_date
+    )
+    if not overdue_borrowings:
+        message = "No borrowings overdue today!"
+        asyncio.run(
+            TelegramNotification().send_telegram_message_to_staff_users(
+                message)
+        )
+    else:
+        for borrowing in overdue_borrowings:
+            message = (
+                f"Overdue Borrowing Alert!\n"
+                f"User: {borrowing.user.email}\n"
+                f"Book: {borrowing.book.title}\n"
+                f"Expected Return Date: {borrowing.expected_return_date}\n"
+            )
+            asyncio.run(
+                TelegramNotification().send_telegram_message_to_staff_users(
+                    message)
+            )
