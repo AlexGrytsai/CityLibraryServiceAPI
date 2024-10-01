0863ad98e2ab34f98fc0b7a1f2fbc43002480cbe
Alex Grytsai <a.grytsai1987@gmail.com>
2024-10-01 17:25:16 +0300

    created PaymentSuccessView and PaymentCancelView & added path to it in urls

--- payment/urls.py
+++ payment/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from payment.views import PaymentView
+from payment.views import PaymentView, payment_success, payment_cancel
 
 router = routers.DefaultRouter()
 
 router.register("payments", PaymentView, basename="payment")
 
 urlpatterns = [
-    path("", include(router.urls)),
+    path("api/v1/", include(router.urls)),
+    path("success/", payment_success, name="payment-success"),
+    path("cancel/", payment_cancel, name="payment-cancel"),
 ]
 
 app_name = "payment"
