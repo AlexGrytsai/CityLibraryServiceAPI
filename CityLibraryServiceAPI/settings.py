b0c1f6d0f6f0d5882f8e2fcbec6172426b1118fe
Alex Grytsai <a.grytsai1987@gmail.com>
2024-10-01 17:16:45 +0300

    Added a session_id to STRIPE_SUCCESS_URL in settings.py

--- CityLibraryServiceAPI/settings.py
+++ CityLibraryServiceAPI/settings.py
@@ -282,5 +282,5 @@ CELERY_BEAT_SCHEDULE = {
 
 STRIPE_PUBLISHABLE_KEY = os.environ.get("STRIPE_PUBLISHABLE_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
-STRIPE_SUCCESS_URL = "http://localhost:8000/success"
+STRIPE_SUCCESS_URL = "http://localhost:8000/success?session_id={CHECKOUT_SESSION_ID}"
 STRIPE_CANCEL_URL = "http://localhost:8000/cancel"
